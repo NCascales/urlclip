47d356c9924a3b795033fa252f1792d1ef9dc486
NICOLÁS CASCALES MARTÍNEZ <Nicolas.Cascales@alu.uclm.es>
2021-12-19 17:57:40 +0100

    changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '@env/environment';
+import { $ } from 'protractor';
 import { finalize } from 'rxjs/operators';
 
-import { QuoteService } from './quote.service';
+import { ShortenerService } from './shortener.service';
 
+interface URLCard {
+  long_url: string;
+  short_url: string;
+  created_At: string;
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,11 +18,14 @@ import { QuoteService } from './quote.service';
 export class HomeComponent implements OnInit {
   quote: string | undefined;
   isLoading = false;
+  inputURL: any = '';
+  URLCard: any;
 
-  constructor(private quoteService: QuoteService) {}
+  constructor(private shortenerService: ShortenerService) {}
 
   ngOnInit() {
     this.isLoading = true;
+    /*
     this.quoteService
       .getRandomQuote({ category: 'dev' })
       .pipe(
@@ -25,6 +35,21 @@ export class HomeComponent implements OnInit {
       )
       .subscribe((quote: string) => {
         this.quote = quote;
+      });*/
+  }
+
+  showURL(url: string) {
+    //llamada de servicio
+    this.shortenerService.postURL(url).subscribe((result: any) => {
+      console.log(result);
+      this.URLCard.push({
+        long_url: result.url,
+        short_url: environment.serverUrl + '/' + result.code,
+        created_at: new Date(result.created_at).toUTCString(),
       });
+    });
+    console.log(url);
+    var newURL = '<ion-label color="primary">' + url + '</ion-label><br>';
+    //$("#resultURL").append(str);
   }
 }
